81f22ce5b37956aa4a6e7aca52b10d7a67072e3e
Aluno <Aluno@GRVL301B-02>
2018-10-05 19:54:19 -0300

    NAVBAR CONFIGURADO localhost umbler

--- application/config/config.php
+++ application/config/config.php
@@ -22,8 +22,8 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/MeuProjetoFrame';
-//$config['base_url'] = 'http://meuprojetoframe-com.umbler.net';
+//$config['base_url'] = 'http://localhost/MeuProjetoFrame';
+$config['base_url'] = 'http://meuprojetoframe-com.umbler.net';
 
 
 /*
